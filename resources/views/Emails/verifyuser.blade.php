f6693c50ba265ccfbc05c6360c3dcc4dbfbc414b
Nikhil Gangaramani <gangaramaninikhil@gmail.com>
2021-03-19 14:20:04 +0530

    First Commit

---
+++ resources/views/Emails/verifyuser.blade.php
@@ -0,0 +1,11 @@
+@component('mail::message')
+    # Hello from CIMS
+
+    Welcome to our Application
+    @component('mail::button', ['url' => 'dashboard'])
+        Verify Email
+    @endcomponent
+
+    Thanks,<br>
+    {{ config('app.name') }}
+@endcomponent
